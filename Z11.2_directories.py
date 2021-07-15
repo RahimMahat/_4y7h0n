adc90b0e4d4006fa762be50f6abd57c5dd97fa85
RahimMahat <85168245+RahimMahat@users.noreply.github.com>
2021-07-15 18:50:50 +0530

    Update Z11.2_directories.py

--- Z11.2_directories.py
+++ Z11.2_directories.py
@@ -11,7 +11,7 @@ def request(url):
 
 target_url = "localhost/mutillidae"
 
-with open("small-filesNdirs.txt","r") as wordlist:
+with open("filesNdirs-wordlist","r") as wordlist:
 	for line in wordlist:
 		word = line.strip() 
 		test_url = target_url + "/" + word
